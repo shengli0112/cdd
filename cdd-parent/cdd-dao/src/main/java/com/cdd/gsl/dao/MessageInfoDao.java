8549d5988679e12b1e5aa52e9808ea6a312d8704
guoshengli <guoshengli@sunlands.com>
2019-04-04 14:00:04 +0800

    error

--- cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/MessageInfoDao.java
+++ cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/MessageInfoDao.java
@@ -1,6 +1,7 @@
 package com.cdd.gsl.dao;
 
 import com.cdd.gsl.domain.MessageInfoDomain;
+import com.cdd.gsl.vo.MessageConditionVo;
 import com.cdd.gsl.vo.MessageVo;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
@@ -12,7 +13,7 @@ public interface MessageInfoDao {
     @Select("select id as messageId,message as message,user_id as userId,entrust_id as entrustId," +
             "house_id as houseId,is_read as isRead,create_ts as createTs from t_message_info where user_id=#{userId} order by create_ts desc" +
             " limit #{from},#{pageSize}")
-    List<MessageVo> messageList(Long userId);
+    List<MessageVo> messageList(MessageConditionVo messageConditionVo);
     @Update("update t_message_info set is_read=1 where id=#{messageId}")
     void updateMessageRead(Long messageId);
 
