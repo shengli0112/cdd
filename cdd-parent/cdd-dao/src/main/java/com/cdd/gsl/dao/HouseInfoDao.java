67c93b8bd4c375eb4af399c78c5d5968da0b6be5
guoshengli <guoshengli@sunlands.com>
2019-04-04 16:29:16 +0800

    touxiang

--- cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/HouseInfoDao.java
+++ cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/HouseInfoDao.java
@@ -390,18 +390,18 @@ public interface HouseInfoDao {
             "and status=1 order by rand() limit 5")
     List<HouseInfoDomainVo> selectHouseInfoListByRecommend(@Param("userId") Long userId);
 
-    @Select("select id as id, title as title, city as city, " +
-            "county as county,town as town, street as street, area as area," +
-            "house_number as houseNumber, selling_price as sellingPrice,concat(electricity,'KV') as electricity," +
-            "(select dict_value from t_common_dict where dict_name='houseType' and dict_code=house_type) as houseType, " +
-            "(select dict_value from t_common_dict where dict_name='houseUseType' and dict_code=house_use_type) as houseUseType, " +
-            "(select dict_value from t_common_dict where dict_name='floor' and dict_code=floor) as floor, " +
-            "(select dict_value from t_common_dict where dict_name='fireControl' and dict_code=fire_control) as fireControl, " +
-            "(select dict_value from t_common_dict where dict_name='priceType' and dict_code=price_type) as priceType, " +
-            "contacts as contacts,phone as phone, background as background, house_status as houseStatus," +
-            "sign_contract as signContract,cover_area as coverArea,house_edge as houseEdge,user_id as userId," +
-            "single_price as singlePrice,use_area as useArea,create_ts as createTs,trade as trade " +
-            " from t_house_info where id=#{houseId} " +
+    @Select("select h.id as id, h.title as title, h.city as city, " +
+            "h.county as county,h.town as town, h.street as street, h.area as area," +
+            "h.house_number as houseNumber, h.selling_price as sellingPrice,concat(h.electricity,'KV') as electricity," +
+            "(select dict_value from t_common_dict where dict_name='houseType' and dict_code=h.house_type) as houseType, " +
+            "(select dict_value from t_common_dict where dict_name='houseUseType' and dict_code=h.house_use_type) as houseUseType, " +
+            "(select dict_value from t_common_dict where dict_name='floor' and dict_code=h.floor) as floor, " +
+            "(select dict_value from t_common_dict where dict_name='fireControl' and dict_code=h.fire_control) as fireControl, " +
+            "(select dict_value from t_common_dict where dict_name='priceType' and dict_code=h.price_type) as priceType, " +
+            "h.contacts as contacts,h.phone as phone, h.background as background, h.house_status as houseStatus," +
+            "h.sign_contract as signContract,h.cover_area as coverArea,h.house_edge as houseEdge,h.user_id as userId," +
+            "h.single_price as singlePrice,h.use_area as useArea,h.create_ts as createTs,h.trade as trade,u.portrait as portrait " +
+            " from t_house_info h left join t_user_info u on h.user_id=u.id where id=#{houseId} " +
             "and status=1")
     HouseInfoDomainVo selectHouseInfoListById(@Param("houseId") Long houseId);
 
