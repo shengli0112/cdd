e0eb3c99ebca77c9989e493553d230e8fab97e41
guoshengli <guoshengli@sunlands.com>
2019-05-07 15:09:10 +0800

    数据源

--- cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/RegionCountyInfoDao.java
+++ cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/RegionCountyInfoDao.java
@@ -1,5 +1,6 @@
 package com.cdd.gsl.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,5 @@ public interface RegionCountyInfoDao {
     @Select("select county_name as countyName from t_region_city_info city " +
             "left join t_region_county_info county on city.city_id=county.city_id " +
             "where city.city_name=#{cityName}")
-    public List<String> findCountyNameByCityName(String cityName);
+    public List<String> findCountyNameByCityName(@Param("cityName") String cityName);
 }
--- cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/RegionTownInfoDao.java
+++ cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/RegionTownInfoDao.java
@@ -1,5 +1,6 @@
 package com.cdd.gsl.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,5 @@ public interface RegionTownInfoDao {
     @Select("select town.town_name as townName from t_region_county_info county " +
             "left join t_region_town_info town on county.county_id=town.county_id " +
             "where county.county_name=#{countyName}")
-    public List<String> findTownNameByCountyName(String countyName);
+    public List<String> findTownNameByCountyName(@Param("countyName") String countyName);
 }
