de58e7982e107bd39272e81ba1d9e0d80fe2e289
guoshengli <guoshengli@sunlands.com>
2019-05-07 15:26:11 +0800

    数据源

--- cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/RegionCountyInfoDao.java
+++ cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/RegionCountyInfoDao.java
@@ -1,15 +1,13 @@
 package com.cdd.gsl.dao;
 
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
-@Repository
+
 public interface RegionCountyInfoDao {
 
     @Select("select county_name as countyName from t_region_city_info city " +
             "left join t_region_county_info county on city.city_id=county.city_id " +
             "where city.city_name=#{cityName}")
-    public List<String> findCountyNameByCityName(@Param("cityName") String cityName);
+    public List<String> findCountyNameByCityName(String cityName);
 }
