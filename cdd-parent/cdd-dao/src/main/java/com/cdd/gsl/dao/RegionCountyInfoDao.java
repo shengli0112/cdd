22eb6afac8a4937987cb13b4231d0a80f65a82c9
guoshengli <guoshengli@sunlands.com>
2019-05-07 15:46:43 +0800

    数据源

--- cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/RegionCountyInfoDao.java
+++ cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/RegionCountyInfoDao.java
@@ -8,6 +8,6 @@ public interface RegionCountyInfoDao {
 
     @Select("select county_name as countyName from t_region_city_info city " +
             "left join t_region_county_info county on city.city_id=county.city_id " +
-            "where city.city_name=#{cityName}")
-    public List<String> findCountyNameByCityName(String cityName);
+            "where city.city_name=#{city}")
+    public List<String> findCountyNameByCityName(String city);
 }
