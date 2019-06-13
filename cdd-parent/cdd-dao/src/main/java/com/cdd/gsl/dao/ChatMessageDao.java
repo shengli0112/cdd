4feb292d7ea3e820df6f2eed1bde7c1d4e4a4b48
guoshengli <guoshengli@sunlands.com>
2019-06-13 13:05:27 +0800

    aa

--- cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/ChatMessageDao.java
+++ cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/ChatMessageDao.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface ChatMessageDao {
     @Select("select id as id,send_user_id as sendUserId,receive_user_id as receiveUserId,obj_id as objId," +
-            "type as type,message_content as messageContent,create_ts as createTs" +
+            "type as type,message_content as messageContent,create_ts as createTs " +
             "from t_chat_message " +
             "where 1=1 and obj_id=#{objId} and type=#{type} and (receive_user_id=#{userId} or send_user_id=#{userId})" +
             " order by create_ts")
