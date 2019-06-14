58438b3ed3251e15b424012bd290ef2f0094d0d8
guoshengli <guoshengli@sunlands.com>
2019-06-14 14:35:48 +0800

    ddd

--- cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/MessageInfoDao.java
+++ cdd-parent/cdd-dao/src/main/java/com/cdd/gsl/dao/MessageInfoDao.java
@@ -11,7 +11,8 @@ import java.util.List;
 public interface MessageInfoDao {
 
     @Select("select id as messageId,message as message,user_id as userId,entrust_id as entrustId," +
-            "house_id as houseId,is_read as isRead,create_ts as createTs,message_type as messageType,obj_id as objId,type as type " +
+            "house_id as houseId,is_read as isRead,create_ts as createTs,message_type as messageType," +
+            "obj_id as objId,type as type,send_user_id as sendUserId,receive_user_id as receiveUserId " +
             "from t_message_info where user_id=#{userId} order by create_ts desc" +
             " limit #{from},#{pageSize}")
     List<MessageVo> messageList(MessageConditionVo messageConditionVo);
