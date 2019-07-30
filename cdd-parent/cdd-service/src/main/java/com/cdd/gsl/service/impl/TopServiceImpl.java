4b87c16400a4dc30e78ea13edeb7514c615e5af3
guoshengli <guoshengli@sunlands.com>
2019-07-30 17:50:16 +0800

    dd

--- cdd-parent/cdd-service/src/main/java/com/cdd/gsl/service/impl/TopServiceImpl.java
+++ cdd-parent/cdd-service/src/main/java/com/cdd/gsl/service/impl/TopServiceImpl.java
@@ -15,9 +15,10 @@ import com.cdd.gsl.vo.TopInfoVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class TopServiceImpl implements TopService {
 
     private Logger logger = LoggerFactory.getLogger(TopServiceImpl.class);
