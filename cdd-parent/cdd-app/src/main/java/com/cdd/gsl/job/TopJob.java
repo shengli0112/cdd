9c62a80a34e615b9455fb2e28a80b5005a5c8a3d
shihui.he <shihui.he@qunar.com>
2019-08-27 23:23:24 +0800

    dd

--- cdd-parent/cdd-app/src/main/java/com/cdd/gsl/job/TopJob.java
+++ cdd-parent/cdd-app/src/main/java/com/cdd/gsl/job/TopJob.java
@@ -4,10 +4,13 @@ import com.cdd.gsl.service.HouseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
+@Lazy(false)
 @EnableScheduling
 @Component
 public class TopJob {
@@ -16,7 +19,7 @@ public class TopJob {
     @Autowired
     private HouseService houseService;
 
-    @Scheduled(cron = "0 0 0 * * ?")
+    @Scheduled(cron = "0/5 * * * * ?")
     public void expireTopHouse(){
         houseService.delayTopHouse();
     }
