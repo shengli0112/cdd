161d5ddb00a10872dc52f16c623b29e38dd404dd
guoshengli <guoshengli@sunlands.com>
2019-09-26 17:09:03 +0800

    定时任务

--- cdd-parent/cdd-app/src/main/java/com/cdd/gsl/job/TopJob.java
+++ cdd-parent/cdd-app/src/main/java/com/cdd/gsl/job/TopJob.java
@@ -19,7 +19,7 @@ public class TopJob {
     @Autowired
     private HouseService houseService;
 
-    @Scheduled(cron = "0 0 0 * * ?")
+    @Scheduled(cron = "0 10 17 * * ?")
     public void expireTopHouse(){
         houseService.delayTopHouse();
     }
